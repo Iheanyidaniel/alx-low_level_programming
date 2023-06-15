17233ab15d764a68bd6c5b7feb7e1941e435d997
Iheanyidaniel <imembaiheanyichukwu@gmail.com>
2023-06-15 12:12:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main: Entry
+ * main -  Starts every C program
  * Return: Always 0
  */
-int main(void) 
-{	
+int main(void)
+	{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+	}
