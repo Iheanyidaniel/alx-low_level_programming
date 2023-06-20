87b78fa747430360a7b752aee674ca238e1a4d0a
Iheanyidaniel <imembaiheanyichukwu@gmail.com>
2023-06-20 11:55:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Print 10x alphabet in lowercase, followed by new line
+*/
+void print_alphabet_x10(void)
+{
+char x;
+int a;
+a = 0;
+while (i < 10)
+{
+x = 'a'
+while (x <= 'z')
+{
+_putchar(x);
+x++
+}
+_putchar('\n');
+a++;
+}
+}
---
+++ 0x02-functions_nested_loops/READEM.md
@@ -0,0 +1,1 @@
+My 10x alphabet task
